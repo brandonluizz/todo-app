d35fba518ba72be62bafdc494138cb44021fba58
Brandon Silva | Cappta <brandonluizz@hotmail.com>
2018-04-23 20:02:39 -0300

    feat: added new template files and actions in form

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.refresh = this.refresh.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+
+    handleAdd() {
+        const description = this.state.description
+
+        axios.post(URL, { description }).then(res => this.refresh())
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=createdAt`).then(res => this.setState({ ...this.state, description: '', list: res.data }))
+    }
+
+    handleChange(event) {
+        this.setState({ ...this.state, description: event.target.value })
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`).then(res => this.refresh())
+    }
+
     render() {
         return (
             <div>
-                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm handleAdd={this.handleAdd} description={this.state.description} handleChange={this.handleChange} />
+                <TodoList list={this.state.list} handleRemove={this.handleRemove} />
             </div>
-        )            
+        )
     }
 }
