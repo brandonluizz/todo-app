16ea92ed28c310739d8579e7c014a5397a2c4aad
Brandon Silva | Cappta <brandonluizz@hotmail.com>
2018-05-21 18:54:21 -0300

    added redux integration with react

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,83 +1,14 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
-const URL = 'http://localhost:3003/api/todos'
+export default props => (
+    <div>
+        <PageHeader name='Tarefas' small='Cadastro' />
+        <TodoForm />
+        <TodoList />
+    </div>
+)
 
-export default class Todo extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = { description: '', list: [] }
-
-        this.handleAdd = this.handleAdd.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.refresh = this.refresh.bind(this)
-        this.handleRemove = this.handleRemove.bind(this)
-        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
-        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
-        this.handleSearch = this.handleSearch.bind(this)
-
-        this.refresh()
-    }
-
-    handleAdd() {
-        const description = this.state.description
-
-        axios.post(URL, { description }).then(res => this.refresh())
-    }
-
-    refresh(description = '') {
-        const search = description ? `&description__regex=/${description}/` : ''
-        axios.get(`${URL}?sort=-createdAt${search}`)
-            .then(res => this.setState({ ...this.state, description, list: res.data }))
-    }
-
-    handleSearch() {
-        this.refresh(this.state.description)
-    }
-
-    handleChange(event) {
-        this.setState({ ...this.state, description: event.target.value })
-    }
-
-    handleRemove(todo) {
-        axios.delete(`${URL}/${todo._id}`)
-            .then(res => this.refresh(this.state.description))
-    }
-
-    handleMarkAsDone(todo) {
-        axios.put(`${URL}/${todo._id}`, { done: true })
-            .then(res => this.refresh(this.state.description))
-    }
-
-    handleMarkAsPending(todo) {
-        axios.put(`${URL}/${todo._id}`, { done: false })
-            .then(res => this.refresh(this.state.description))
-    }
-
-    handleClear() {
-        this.refresh()
-    }
-
-    render() {
-        return (
-            <div>
-                <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm handleAdd={this.handleAdd}
-                    description={this.state.description}
-                    handleChange={this.handleChange}
-                    handleSearch={this.handleSearch}
-                    handleClear={this.handleClear.bind(this)} />
-                <TodoList
-                    list={this.state.list}
-                    handleRemove={this.handleRemove}
-                    handleMarkAsDone={this.handleMarkAsDone}
-                    handleMarkAsPending={this.handleMarkAsPending} />
-            </div>
-        )
-    }
-}
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending, remove } from './todoActions'
 
 const Todo = props => {
     const renderRows = () => {
@@ -10,12 +13,12 @@ const Todo = props => {
             <tr key={todo._id}>
                 <td className={todo.done ? 'markAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='success' icon='check' hide={todo.done} 
-                    onClick={() => props.handleMarkAsDone(todo)}/>
-                    <IconButton style='warning' icon='undo' hide={!todo.done} 
-                    onClick={() => props.handleMarkAsPending(todo)}/>
-                    <IconButton style='danger' icon='trash-o' hide={!todo.done}  
-                    onClick={() => props.handleRemove(todo)}/>
+                    <IconButton style='success' icon='check' hide={todo.done}
+                        onClick={() => props.markAsDone(todo)} />
+                    <IconButton style='warning' icon='undo' hide={!todo.done}
+                        onClick={() => props.markAsPending(todo)} />
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
+                        onClick={() => props.remove(todo)} />
                 </td>
             </tr>
         ))
@@ -37,5 +40,6 @@ const Todo = props => {
 }
 
 const mapStateToProps = state => ({ list: state.todo.list })
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
 
-export default connect(mapStateToProps)(Todo)
+export default connect(mapStateToProps, mapDispatchToProps)(Todo)
